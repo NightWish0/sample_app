5c844837da34a509a7da59824292745d04cf9769
XuXin <xuxin@dingyantech.com>
2018-03-26 14:58:54 +0800

    Rails 使用session 方法在临时 cookie 中维护页面之间的状态；  • 登录表单的目的是创建新会话，登入用户；  • flash.now 方法用于在重新渲染的页面中显示闪现消息；  • 在测试中重现问题时可以使用测试驱动开发；  • 使用session 方法可以安全地在浏览器中存储用户 ID，创建临时会话；  • 可以根据登录状态修改功能，例如布局中显示的链接；  • 集成测试可以检查路由、数据库更新和对布局的修改；  8.4

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  # session相关
+  get '/login',to: 'sessions#new'
+  post '/login',to: 'sessions#create'
+  delete '/logout',to: 'sessions#destroy'
+
+  # 创建用户
   get '/signup',to: 'users#new'
 
   root 'static_pages#home'
